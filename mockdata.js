7a51904e4590601354507fc20f76b38ddc332692
NielRms22 <126554247+NielRms22@users.noreply.github.com>
2023-05-30 15:45:53 +0800

    Add files via upload

---
+++ mockdata.js
@@ -0,0 +1,31 @@
+let users = {
+    firstname: "Niel",
+    lastname: "Ramos",
+    email: "xenpie@gmail.com",
+    password: "*****",
+    mobilenum: "09543367824",
+}
+
+let orderproduct = {
+    orderID: 1111,
+    productID: 2345,
+    quantity: 1,
+    price: 5499,
+    subtotal: 5499,
+}
+
+let products = {
+    name:"Logitech G Pro X Superlight",
+    desc: "Gaming Mouse",
+    price: 5499,
+    stocks: 20,
+    isActive: true,
+    SKU: "BM452351",
+}
+
+let orders = {
+    userID: 902341,
+    transactionDate: "May 20, 2023",
+    Status: "In transport",
+    total: 5499,
+}
